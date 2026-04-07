0df2299f8541a6c082cdd9c5ee4d5ae65bb2b037
Bedirhan Taha SARI <bedirhantahasari@gmail.com>
2026-04-07 09:17:33 +0300

    Finans sayfasının boyutları ayarlandı.

--- desktop/src/renderer/src/components/Layout.jsx
+++ desktop/src/renderer/src/components/Layout.jsx
@@ -4,11 +4,11 @@ import TopBar from './TopBar'
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen bg-page-bg text-on-surface">
+    <div className="flex h-screen bg-page-bg text-on-surface overflow-hidden">
       <Sidebar />
-      <div className="flex-1 ml-64 flex flex-col min-h-screen">
+      <div className="flex-1 ml-64 flex flex-col overflow-hidden">
         <TopBar />
-        <main className="flex-1 overflow-auto">
+        <main className="flex-1 min-h-0 overflow-auto">
           <Outlet />
         </main>
       </div>
