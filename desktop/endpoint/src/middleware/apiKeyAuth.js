a73bacb1b8c9cdb6561e96a53a520fe792dd8792
Bedirhan Taha SARI <bedirhantahasari@gmail.com>
2026-05-20 23:24:15 +0300

    Remove endpoint folder

--- desktop/endpoint/src/middleware/apiKeyAuth.js
+++
@@ -1,9 +0,0 @@
-function apiKeyAuth(req, res, next) {
-  const key = req.headers['x-api-key']
-  if (!key || key !== process.env.ENDPOINT_API_KEY) {
-    return res.status(401).json({ error: 'Invalid or missing API key' })
-  }
-  next()
-}
-
-module.exports = { apiKeyAuth }
